9c0995d96b1dee0f7ce4f99f3559c75bee9e0fce
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-02-16 22:50:35 +0100

    Order

---
+++ src/main/java/com/shop/soteria/data/model/Order.java
@@ -0,0 +1,10 @@
+package com.shop.soteria.data.model;
+
+import java.time.LocalDateTime;
+
+public class Order {
+    private String status;
+    private double total;
+    private LocalDateTime orderDate;
+    private LocalDateTime deliveryDate;
+}
