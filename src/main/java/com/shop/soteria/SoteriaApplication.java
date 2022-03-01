fe40f860bcdb9297a1e72c7b88e73f9fc5fac45f
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-03-01 22:00:39 +0100

    update

---
+++ src/main/java/com/shop/soteria/SoteriaApplication.java
@@ -0,0 +1,13 @@
+package com.shop.soteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoteriaApplication.class, args);
+	}
+
+}
