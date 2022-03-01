ab9ad8c923d84453a4c6e7804f9bc189a0b55ad3
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-03-01 22:01:22 +0100

    update

---
+++ src/test/java/com/shop/soteria/SoteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.soteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
