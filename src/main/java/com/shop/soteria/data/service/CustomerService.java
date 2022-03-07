509f68f9775a79cc3c9b41f28f4c1d587a5efa07
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-03-07 10:30:55 +0100

    update

--- src/main/java/com/shop/soteria/data/service/CustomerService.java
+++ src/main/java/com/shop/soteria/data/service/CustomerService.java
@@ -1,8 +1,10 @@
 package com.shop.soteria.data.service;
 
+import com.shop.soteria.data.model.Customer;
 import com.shop.soteria.data.model.Product;
 
 public interface CustomerService {
+    Customer signUp();
     boolean addToCart(Product prodName);
     boolean removeFromCart(Product product);
 }
--- src/main/java/com/shop/soteria/data/service/CustomerServiceImpl.java
+++ src/main/java/com/shop/soteria/data/service/CustomerServiceImpl.java
@@ -1,2 +1,30 @@
-package com.shop.soteria.data.service;public class CustomerServiceImpl {
+package com.shop.soteria.data.service;
+
+import com.shop.soteria.data.model.Customer;
+import com.shop.soteria.data.model.Product;
+import com.shop.soteria.data.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerServiceImpl implements CustomerService{
+
+    @Autowired
+    CustomerRepository customerRespository;
+
+    @Override
+    public Customer signUp() {
+
+    }
+
+    @Override
+    public boolean addToCart(Product prodName) {
+
+        return false;
+    }
+
+    @Override
+    public boolean removeFromCart(Product product) {
+        return false;
+    }
 }
