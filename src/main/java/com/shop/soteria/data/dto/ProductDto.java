63f2178e4f036a89fd77dc1d0c339e2cdf100772
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-03-17 15:33:37 +0100

    dto

--- src/main/java/com/shop/soteria/data/dto/ProductDto.java
+++ src/main/java/com/shop/soteria/data/dto/ProductDto.java
@@ -1,4 +1,10 @@
 package com.shop.soteria.data.dto;
 
+import lombok.Data;
+
+@Data
 public class ProductDto {
+    private String name;
+    private String description;
+    private double price;
 }
