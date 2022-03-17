5d8fe54bc6896f9fcf286eed43422c08253fbf77
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-03-17 05:33:42 +0100

    service

--- src/main/java/com/shop/soteria/data/service/productService/ProductService.java
+++ src/main/java/com/shop/soteria/data/service/productService/ProductService.java
@@ -1,2 +1,15 @@
-package com.shop.soteria.data.service.productService;public class ProductServvice {
+package com.shop.soteria.data.service.productService;
+
+import com.shop.soteria.data.dto.ProductDto;
+import com.shop.soteria.data.model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    void addProduct();
+    Product getProduct();
+    void deleteProduct();
+    ProductDto updateProduct();
+    List<Product> getAllProduct();
+    List<Product> getAllProduct(Long merchantNo);
 }
