d0dc48dd4a2197eba3b480a4c4759732301b68b6
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-03-21 18:44:28 +0100

    model

--- src/main/java/com/shop/soteria/data/model/User.java
+++ src/main/java/com/shop/soteria/data/model/User.java
@@ -1,15 +1,15 @@
 package com.shop.soteria.data.model;
 
-import com.sun.istack.NotNull;
-
-import javax.annotation.processing.Generated;
+import com.shop.soteria.data.enums.Roles;
 import javax.persistence.*;
 
 @Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String name;
     private String email;
     private String password;
+    private Roles role;
 }
