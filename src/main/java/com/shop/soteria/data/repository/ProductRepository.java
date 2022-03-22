ff039b398da35a827f05f20a368653e6201ee294
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-03-22 15:33:02 +0100

    update

--- src/main/java/com/shop/soteria/data/repository/ProductRepository.java
+++ src/main/java/com/shop/soteria/data/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.shop.soteria.data.repository;
 import com.shop.soteria.data.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> getProductsByMerchantId(Long merchantNo);
 }
--- src/main/java/com/shop/soteria/data/repository/UserRepository.java
+++ src/main/java/com/shop/soteria/data/repository/UserRepository.java
@@ -1,10 +1,9 @@
 package com.shop.soteria.data.repository;
 
-import com.shop.soteria.data.model.Customer;
+import com.shop.soteria.data.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 
-public interface CustomerRepository extends JpaRepository <Customer, Long> {
+public interface UserRepository extends JpaRepository <User, Long> {
 
 }
