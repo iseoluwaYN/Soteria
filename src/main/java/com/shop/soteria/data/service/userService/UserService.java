90dbb1d124fbafeeb85605f038e17d05822ba4e1
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-03-22 16:12:07 +0100

    service

--- src/main/java/com/shop/soteria/data/service/userService/UserService.java
+++ src/main/java/com/shop/soteria/data/service/userService/UserService.java
@@ -3,4 +3,6 @@ package com.shop.soteria.data.service.userService;
 import com.shop.soteria.data.model.User;
 
 public interface UserService {
+    User register(User user);
+
 }
--- src/main/java/com/shop/soteria/data/service/userService/UserServiceImpl.java
+++ src/main/java/com/shop/soteria/data/service/userService/UserServiceImpl.java
@@ -1,2 +1,4 @@
-package com.shop.soteria.data.service.userService;public class UserServiceImpl {
+package com.shop.soteria.data.service.userService;
+
+public class UserServiceImpl {
 }
