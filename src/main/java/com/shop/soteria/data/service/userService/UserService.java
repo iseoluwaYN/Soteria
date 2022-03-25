4570c205a7ca1bcb1536f5b44339c0aeb24e108b
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-03-25 10:51:01 +0100

    service

--- src/main/java/com/shop/soteria/data/service/userService/UserService.java
+++ src/main/java/com/shop/soteria/data/service/userService/UserService.java
@@ -1,8 +1,14 @@
 package com.shop.soteria.data.service.userService;
 
+import com.shop.soteria.data.enums.Roles;
 import com.shop.soteria.data.model.User;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public interface UserService {
     User register(User user);
-
+    List<User> getAllUsers();
+    List<User> getAllUsers(Roles role);
 }
--- src/main/java/com/shop/soteria/data/service/userService/UserServiceImpl.java
+++ src/main/java/com/shop/soteria/data/service/userService/UserServiceImpl.java
@@ -1,4 +1,32 @@
 package com.shop.soteria.data.service.userService;
 
-public class UserServiceImpl {
+import com.shop.soteria.data.enums.Roles;
+import com.shop.soteria.data.model.User;
+import com.shop.soteria.data.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    @Override
+    public User register(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public List<User> getAllUsers(Roles role) {
+        return null;
+    }
 }
